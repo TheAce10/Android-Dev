917b0740e5a795d3e632fd88a2247d513bd64136
Elikem <99412803+TheAce10@users.noreply.github.com>
2024-06-30 03:05:32 +0000

    git push from local

---
+++ Basics/1.kt
@@ -0,0 +1,88 @@
+
+fun main() {
+    println("Hello World!")
+    // fun calculater() {
+    // }
+
+    val count: Int = 2
+    println(count)
+
+    // var ccc = 1115
+    // var v: Double = 7.5
+    Float
+    // var ok: Boolean = true
+}
+
+fun calc() {
+    println("Play")
+    // var qo: Double
+    println("me")
+    println("Radio")
+}
+/*
+fun main() { // Youtube Channel
+    var new: Int = 1 //in-line comment
+    var channelName: string = "Ace Bek"
+    var subcriberCount = 0
+    var subcribed = false
+    var like = false
+    var dislike = false
+    /*
+    *multi
+    *line
+    *comment
+    /
+
+    println("Welcome! to $channelName,\n" + "This channel has $subcriberCount subcribers")
+    println("Your current subcription state is \"$subscribed\" and like is \"$like\" and dislike is \"$dislike\"")
+
+}
+
+fun main() {
+    birthdayGreeting()
+}
+
+fun birthdayGreeting(name:string= "") {
+    println("Happy Birthday $name")
+}
+
+// function to return
+fun adder(num:Int= 0) : Int {
+    return num + 9
+}
+
+// function parameters are values a function should take i.e. like a template
+// when a value is passed to an instance of a function that is an argument
+//parameter list, function signature
+//named arguments are used to specify which parameter recieves the value witout the need for ordering
+
+fun base(name: String= "",score: Double=0.00): Unit {
+
+}
+
+// function call
+base(name= "Josh", score= 20.6)
+// this canbe rewritten as 
+base(score= 20.6, name= "Josh")//performs same function
+// one advantage of this is when using default arguments
+// eg if we want to use default name but specify score
+base(20.6) // this won't work because. the argument intended for score will be assigned to name which raises an error
+base(score = 20.6)//this on th other hand assigns value to score and use default name argument
+
+
+// 10
+fun main() {
+    var timeSpentToday = 300
+    var timeSpentYesterday = 250
+    println(comP(timeSpentToday, timeSpentYesterday))
+    print()
+}
+fun comP(a: Int, b: Int): Boolean{
+    return a > b
+}
+
+// 11
+ fun (a: String, b: Int, c: Int, d: Int): Unit {
+    println
+ }
+ */
